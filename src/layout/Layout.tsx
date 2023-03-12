b1e54273702bd40688e818b923339865a5071820
jiwonmik <jiwonmik@gmail.com>
2023-03-12 17:47:03 +0900

    fix: ToastContainer 컴포넌트 위치 변경 1. Layout 컴포넌트에서 렌더링 되도록 변경

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,5 +1,6 @@
 import { Flex } from '@chakra-ui/react';
 import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
 import Header from '../components/Header';
 
@@ -9,6 +10,19 @@ function Layout() {
       <Flex flexDirection={'column'}>
         <Header />
       </Flex>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        limit={2}
+      />
       <Outlet />
     </>
   );
